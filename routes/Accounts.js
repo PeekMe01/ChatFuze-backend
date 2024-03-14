a01b033600ca70b3343525173585bcd45ee910d0
RalpnAntoine <ralphdaher6@gmail.com>
2024-03-14 22:50:22 +0200

    OTP
    Added otp functions

--- routes/Accounts.js
+++ routes/Accounts.js
@@ -1,41 +1,12 @@
 const express = require('express')
 //const bcrypt = require('bcrypt');
+const nodemailer = require('nodemailer');
 const {Users}=require('../models');
 const router=express.Router()
 
 router.get('/', (req, res) => {
     res.send("account server");
 });
-
-
-router.post('/validate_username', async (req, res) => {
-  const { tmpUsername } = req.body;
-  const users = await Users.findAll({});
-  let available=true;
-  users.forEach(user => {
-      if (user.username === tmpUsername) {
-        available=false;
-      }
-  });
-  
-  return res.json({available:available});
-});
-
-router.post('/validate_email', async (req, res) => {
-  const { tmpEmail } = req.body;
-  const users = await Users.findAll({});
-  let available=true;
-  users.forEach(user => {
-      if (user.email === tmpEmail) {
-        available=false;
-      }
-  });
-  
-  return res.json({available:available});
-});
-
-
-
 
 router.post('/login', async (req, res) => {
     try {
@@ -54,7 +25,7 @@ router.post('/login', async (req, res) => {
     }
 });
   
-  router.post('/register', async (req, res) => {
+  router.post('/Register', async (req, res) => {
     const { email, username, password, dateOfBirth, country, gender } = req.body;
     try {
         await Users.create({
@@ -70,5 +41,48 @@ router.post('/login', async (req, res) => {
       res.status(511).json({ error: "Failed to create user" });
     }
   });
+
+  // Function to generate OTP
+  function generateOTP() {
+    const otpLength = 6;
+    return Math.floor(100000 + Math.random() * 900000).toString().substr(0, otpLength);
+  }
+
+  // Configure nodemailer transporter
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'ralphdaher6@gmail.com', // Your Gmail email address
+        pass: 'bxgo qxdn rwts jwdb' // Your Gmail password
+    }
+  });
+
+    // New route to send OTP to email and return it back to frontend
+  router.post('/sendOTP', async (req, res) => {
+    const { email } = req.body;
+
+    try {
+        // Generate OTP
+        const otp = generateOTP();
+
+        // Send OTP to email
+        const mailOptions = {
+            from: 'ralphdaher6@gmail.com',
+            to: email,
+            subject: 'Your OTP for registration',
+            text: `Your OTP is: ${otp}`
+        };
+
+        await transporter.sendMail(mailOptions);
+
+        console.log('sending otp...')
+
+        // Send OTP back to frontend
+        res.json({ otp: otp, message: 'OTP sent successfully' });
+    } catch (error) {
+        console.error('Error sending OTP:', error);
+        res.status(500).json({ error: 'Failed to send OTP' });
+    }
+});
   
 module.exports=router
