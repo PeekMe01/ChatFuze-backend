116822c18aec89780fe2f7e21ac8e58338aa129c
RalpnAntoine <ralphdaher6@gmail.com>
2024-03-18 18:47:13 +0200

    added status
    added status so i can use in front end

--- routes/Accounts.js
+++ routes/Accounts.js
@@ -93,7 +93,7 @@ router.post('/login', async (req, res) => {
     service: 'gmail',
     auth: {
         user: 'antoinekamel0@gmail.com', // Your Gmail email address
-        pass: 'zxhj sjbh nhuw waau' // Your Gmail password
+        pass: 'zxhj sjbh nhuw waau' // Your Gmail password // Kifak Jawad
     }
   });
   router.post('/sendOTP', async (req, res) => {
@@ -124,7 +124,7 @@ async function sendResetPasswordEmail(email, token) {
     from: 'antoinekamel0@gmail.com',
     to: email,
     subject: 'Reset Your Password',
-    text: `Click the following link to reset your password: https://example.com/resetpassword/${token}`
+    text: `Click the following link to reset your password: http://192.168.0.102:3000/resetpassword/${token}`
   };
   await transporter.sendMail(mailOptions);
 }
@@ -133,12 +133,12 @@ router.post('/resetpassword', async (req, res) => {
     const { email } = req.body;
     const user = await Users.findOne({ where: { email : email} });
     if (!user) {
-      return res.json( 'User not found' );
+      return res.status(500).json({ error: 'User not found' });
     }
     const token = generateUniqueToken();
     await ResetPassword.create({email: email,token: token, userid: user.idusers });
     await sendResetPasswordEmail(email, token);
-    return res.json( 'Reset password email sent successfully' );
+    return res.status(200).json({message: 'Reset password email sent successfully' });
   } catch (error) {
       return res.status(500).json({ error: 'Internal server error: ' + error.message });
     }
@@ -148,24 +148,26 @@ router.post('/resetpassword/:token', async (req, res) => {
     const { token } = req.params;
     const { password, confirmPassword } = req.body;
     if (password != confirmPassword) {
-      return res.json( 'The password and confirm password do not match');
+      return res.status(404).json( {error: 'The password and confirm password do not match'});
     }
     const resetPasswordRecord = await ResetPassword.findOne({ where: { token:token } });
-    if (!resetPasswordRecord) {
-      return res.json('Invalid or expired token');
+    console.log(resetPasswordRecord)
+    if (resetPasswordRecord==null) {
+      return res.status(404).json({ error: 'Invalid or expired token'});
     }
+
     const affectedRows = await Users.update(
       { password: password },
       { where: { email: resetPasswordRecord.email } }
     );
     if (affectedRows > 0) {
       await resetPasswordRecord.destroy();
-      return res.json('Password updated successfully');
+      return res.status(200).json({message: 'Password updated successfully'});
     }else {
-      return res.json('Password not updated');
+      return res.status(500)({error: 'Password not updated'});
     }
   } catch (error) {
-    return res.json('Internal server error');
+    return res.status(500)({error: 'Internal server error'});
   }
 });  
 
